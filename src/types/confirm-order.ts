09e51371e09a38e7512a3e9062afa913748a6985
Gabriel Rabelo <gabriel.rabelo@ozmap.com.br>
2025-04-16 15:07:55 -0300

    feat: add confirm order input and output schemas

---
+++ src/types/confirm-order.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+import { ResourcePoolManagementOutputSchema } from './resource-pool-management';
+
+
+export const ConfirmOrderInputSchema = z.object({
+    port: z.object({
+        idExternal: z.string(),
+        idArea: z.string(),
+        idOperator: z.string(),
+        nameExternal: z.string(),
+    }),
+    idClient: z.string(),
+    codeOrder: z.string(),
+    idsExternals: z.object({
+        gis: z.object({
+            idClientExternal: z.string(),
+        }),
+        idBox: z.string(),
+        idSplitter: z.string(),
+        boxCoordinates: z.object({
+            lat: z.number(),
+            lng: z.number(),
+        }),
+    }),
+});
+
+export const ConfirmOrderOutputSchema =  ResourcePoolManagementOutputSchema
+
+export type ConfirmOrderInput = z.infer<typeof ConfirmOrderInputSchema>;
+export type ConfirmOrderOutput = z.infer<typeof ResourcePoolManagementOutputSchema>;
