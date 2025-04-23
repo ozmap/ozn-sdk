153364d9325774f4b7d8deaf97f6939e4de67f0f
Gabriel <151649951+Gabriiel-Rabeloo@users.noreply.github.com>
2025-04-23 13:54:40 -0300

    [OZN 1075] [OZn-API] Criação de reserva pelo ozloc (#3)
    * feat: add confirm order input and output schemas
    
    * feat: update version and add address field to confirm order schema
    
    * feat: updated confirmOrder output schema
    
    * chore: bump version

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ export * from './src/types/resource-pool-management';
 export * from './src/types/geographic-address';
 export * from './src/types/service-qualification';
 export * from './src/types/shared';
+export * from './src/types/confirm-order';
 export * as Auth from './src/types/auth';
 
---
+++ src/types/confirm-order.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+export const ConfirmOrderInputSchema = z.object({
+    port: z.object({
+        idExternal: z.string(),
+        idArea: z.string(),
+        idOperator: z.string(),
+        nameExternal: z.string(),
+    }),
+    address: z.string(),
+    idClient: z.string(),
+    codeOrder: z.string(),
+    idsExternals: z.object({
+        gis: z.object({
+            idClientExternal: z.string(),
+        }),
+        idBox: z.string(),
+        idSplitter: z.string(),
+        boxCoordinates: z.object({
+            lat: z.number(),
+            lng: z.number(),
+        }),
+    }),
+});
+
+export const ConfirmOrderOutputSchema =  z.object({
+    _id: z.string(),
+});
+
+export type ConfirmOrderInput = z.infer<typeof ConfirmOrderInputSchema>;
+export type ConfirmOrderOutput = z.infer<typeof ConfirmOrderOutputSchema>;
