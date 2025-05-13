f94d9ba6d75dc4ea744e562d63c11f62e63af7c8
Gabriel Rabelo <gabriel.rabelo@ozmap.com.br>
2025-05-13 17:04:30 -0300

    OZN-1066 feat #time 5m update iframeViability to string

--- src/types/service-qualification.ts
+++ src/types/service-qualification.ts
@@ -64,7 +64,7 @@ export const RelatedEntitySchema = z.object({
 const ServiceSchema = z.object({
     serviceCharacteristic: z.array(CharacteristicSchema),
     relatedEntity: z.array(RelatedEntitySchema),
-    iframeViability: z.boolean(),
+    iframeViability: z.string().optional(),
 });
 
 const ServiceQualificationItemSchema = z.object({
